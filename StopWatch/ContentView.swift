5327506c647bfe153f160427205c461d76e9b5b6
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-07-17 20:55:52 +0900

    完成

--- StopWatch/ContentView.swift
+++ StopWatch/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var timer: Timer!
+    @State var secondsElapsed: Double = 0.0
+    @State var isRunning: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(String(format: "%.2f", secondsElapsed))
+                .font(.title)
+            
+            HStack {
+                if isRunning { // 稼働している時
+                    
+                    Button {
+                        pause()
+                    } label: {
+                        Image(systemName: "pause.fill")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .padding()
+                            .background(.orange)
+                            .clipShape(.circle)
+                    }
+                } else { // 静止している時
+                    
+                    Button {
+                        start()
+                    } label: {
+                        Image(systemName: "play.fill")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .padding()
+                            .background(.green)
+                            .clipShape(.circle)
+                    }
+                    
+                }
+                
+                if secondsElapsed != 0.0 {
+                    
+                    Button {
+                        stop()
+                    } label: {
+                        Image(systemName: "stop.fill")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .padding()
+                            .background(.red)
+                            .clipShape(.circle)
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    private func start() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            secondsElapsed += 0.1
+        }
+        isRunning = true
+    }
+    
+    private func pause() {
+        timer.invalidate()
+        isRunning = false
+    }
+    
+    private func stop() {
+        timer.invalidate()
+        isRunning = false
+        secondsElapsed = 0.0
     }
 }
 
