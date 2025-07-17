62c874500328487f6fdc7a447f6130656d99dacf
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-07-17 20:38:07 +0900

    Initial Commit

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by A S on 2025/07/17.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StopWatchTests/StopWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchTests.swift
+//  StopWatchTests
+//
+//  Created by A S on 2025/07/17.
+//
+
+import Testing
+@testable import StopWatch
+
+struct StopWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
